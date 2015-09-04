dd01edb19a939dcc28895bea3189ef894cbe2d9f
NoTalentGeek <mikael.pratama@yahoo.com>
2015-09-04 04:42:43 +0200

    Prototype room 'class'.

--- Game.js
+++ Game.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(1024, 586, Phaser.AUTO);
+var game = new Phaser.Game(1031, 586, Phaser.AUTO);
     game.state.add  ('StateBoot',       stateBoot);
     game.state.add  ('StatePreload',    statePreload);
     game.state.add  ('StateMain',       stateMain);
--- StatePreload.js
+++ StatePreload.js
@@ -2,8 +2,11 @@ var statePreload = {
 
     preload:            function(){
 
-        game.load.spritesheet('SsButton1', 'button1.png', 100, 100);
-        game.load.spritesheet('SsButton2', 'button2.png', 100, 100);
+        game.load.image      ('ImageFrame1' , 'frame1.png');
+        game.load.image      ('ImageFrame2' , 'frame2.png');
+        game.load.image      ('ImageFrame2' , 'frame3.png');
+        game.load.spritesheet('SsButton1'   , 'button1.png', 100, 100);
+        game.load.spritesheet('SsButton2'   , 'button2.png', 100, 100);
 
     },
     create:             function(){},
